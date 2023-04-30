c1730bd999d65c4cae5a58f828334362f2d57116
Arsalan Rabdanov <uzarsalan@gmail.com>
2023-04-30 16:40:19 +0300

    created index.html + script.js

---
+++ script.js
@@ -0,0 +1,59 @@
+let mySonAge;
+console.log(mySonAge);
+mySonAge = 20;
+
+console.log(mySonAge);
+
+mySonAge = 30;
+
+console.log(mySonAge);
+
+const YEAR_OF_BIRTH = 1989;
+
+console.log(YEAR_OF_BIRTH);
+
+let myName = "Ars";
+let name2 = "Arsalan";
+let bio = `
+Привет, ${myName}!
+Rabdanov
+Поздравляем тебя с твоим ${2023 - YEAR_OF_BIRTH}-м днем рождения!
+`;
+
+let isBoy = true;
+
+let isDog = false;
+
+let nothing = null;
+let nothing2 = undefined;
+
+console.log(bio);
+
+console.log(typeof myName);
+
+console.log(1989 + 34);
+console.log(2023 - 34);
+console.log(34 * 12);
+console.log(34 / 10);
+console.log(34 % 10);
+console.log(18 ** 2);
+
+console.log("Arsalan" + " " + "Rabdanov");
+console.log("Arsalan" + " " - "Rabdanov");
+
+let bio2 = "Привет, ";
+bio2 += myName;
+
+console.log(bio2);
+
+mySonAge = mySonAge + 1;
+
+mySonAge += 1;
+
+console.log(mySonAge--);
+
+console.log(mySonAge);
+
+console.log(--mySonAge);
+
+console.log(mySonAge);
